33e8a6b139c0535063461bc783e5e15d47de792b
MTS Standish <16389869+mattThurstan@users.noreply.github.com>
2022-07-30 00:23:00 +0100

    v0.1.0 - Init

---
+++ MT_LightControlDevice.cpp
@@ -0,0 +1,11 @@
+/*
+ * MT_LightControlDevice.cpp - Standard device for Light Control network.
+ * Created by MTS Standish (mattThurstan), 2022.
+ */
+ 
+ #include "Arduino.h"
+ #include "MT_LightControlDevice.h"
+ 
+ MT_LightControlDevice::MT_LightControlDevice() {
+	
+ }
---
+++ MT_LightControlDevice.h
@@ -0,0 +1,24 @@
+/*
+ * MT_LightControlDevice.h - Standard device for Light Control network.
+ * Created by MTS Standish (mattThurstan), 2022.
+ */
+
+#ifndef __MT_LIGHTCONTROLDEVICE_H__
+#define __MT_LIGHTCONTROLDEVICE_H__
+
+#include "Arduino.h"
+
+/*----------------------------main header declarations----------------------------*/
+
+class MT_LightControlDevice
+{
+  protected:
+	
+  private:
+  
+  public:
+	MT_LightControlDevice();
+	
+};
+
+#endif
